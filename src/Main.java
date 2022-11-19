1efa10e519ceae2cc243055db168c93c889475bd
Sap004 <sghosh1441@gmail.com>
2022-11-19 11:48:59 +0530

    2 flavours of multiThreading - updated

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Inside Main!");
         MultiThreadingExtendingThreadClass T1=new MultiThreadingExtendingThreadClass();
-        MultiThreadingImplementingRunnableInterface T2=new MultiThreadingImplementingRunnableInterface();
-        T1.run();
-        T2.run();
+        Thread T2 = new Thread(new MultiThreadingImplementingRunnableInterface());
+        T1.start();
+        T2.start();
 
     }
 }
